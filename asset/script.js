90b6a7674f993b3c1df39a2e1b8f5fc9f56eadfe
Chue Chang <chuechang1996@outlook.com>
2022-03-19 14:22:24 -0500

    add style and js function

--- asset/script.js
+++ asset/script.js
@@ -1,61 +1,87 @@
-var wins = 0;
-var ties = 0;
-var losses = 0;
+function hideShow() {
+    var x = document.querySelector(".rule-message-box")
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else x.style.display = "none"
+}
+var playerScore = document.getElementById("player-score")
+playerScore.innerHTML = 0
+var computerScore = document.getElementById("computer-score")
+computerScore.innerHTML = 0
+var tieScore = document.getElementById("tie-score")
+tieScore.innerHTML = 0
+var playerChoice = ""
+var computerChoice = Math.floor(Math.random() * 3)
 
-// Array of options for computer to pick from
-var options = ["R", "P", "S"];
-
-var playGame = function() {
-  // Ask user for their choice
-  var userChoice = window.prompt("Enter R, P, or S:");
-
-  // If user pressed Cancel, immediately end function
-  if (!userChoice) {
-    return;
-  }
-
-  // Convert to uppercase to make comparisons easier
-  userChoice = userChoice.toUpperCase();
-
-  // Get random index from array of options
-  var index = Math.floor(Math.random() * options.length);
-  var computerChoice = options[index];
-
-  window.alert("The computer chose " + computerChoice);
-
-  // If choices are the same, it's a tie
-  if (userChoice === computerChoice) {
-    ties++;
-    window.alert("It's a tie!");
-
-  // Check every win condition for the player
-  } else if (
-    (userChoice === "R" && computerChoice === "S") || 
-    (userChoice === "P" && computerChoice === "R") || 
-    (userChoice === "S" && computerChoice === "P")
-  ) {
-    wins++;
-    window.alert("You win!");
-
-  // If above conditions failed, assume player lost
-  } else {
-    losses++;
-    window.alert("You lost!");
-  }
-
-  // Print stats with line breaks
-  window.alert(
-    "Stats:\nWins: " + wins + "\nLosses: " + losses + "\nTies: " + ties
-  );
-
-  // Ask user to play again
-  var playAgain = window.confirm("Play again?");
-
-  // If user pressed OK, run the function again
-  if (playAgain) {
-    playGame();
-  }
-};
-
-// Run the game for the first time
-playGame();
+var rockOption = document.querySelector(".rock")
+rockOption.addEventListener("click", function () {
+    var computerChoice = Math.floor(Math.random() * 3);
+    var computerPick = document.getElementById('computer-choice')
+    if (computerChoice == 0) {
+        computerPick.setAttribute('src', 'rock.jpg')
+        document.getElementById('computer-message').innerText = "Computer picked ROCK"
+    } else if (computerChoice == 1) {
+        computerPick.setAttribute('src', 'paper.jpg')
+        document.getElementById('computer-message').innerText = "Computer picked PAPER"
+    } else {
+        computerPick.setAttribute('src', 'scissor.jpg')
+        document.getElementById('computer-message').innerText = "Computer picked SCISSOR"
+    }
+    playerChoice = 0;
+    document.getElementById('player-message').innerText = "You picked ROCK"
+    if (computerChoice == playerChoice) {
+        tieScore.innerText++
+    } else if (computerChoice == 1) {
+        computerScore.innerText++
+    } else {
+        playerScore.innerText++
+    }
+})
+var paperOption = document.querySelector(".paper")
+paperOption.addEventListener("click", function () {
+    var computerChoice = Math.floor(Math.random() * 3);
+    var computerPick = document.getElementById('computer-choice')
+    if (computerChoice == 0) {
+        computerPick.setAttribute('src', 'rock.jpg')
+        document.getElementById('computer-message').innerText = "Computer picked ROCK"
+    } else if (computerChoice == 1) {
+        computerPick.setAttribute('src', 'paper.jpg')
+        document.getElementById('computer-message').innerText = "Computer picked PAPER"
+    } else {
+        computerPick.setAttribute('src', 'scissor.jpg')
+        document.getElementById('computer-message').innerText = "Computer picked SCISSOR"
+    }
+    playerChoice = 1;
+    document.getElementById('player-message').innerText = "You picked PAPER"
+    if (computerChoice == playerChoice) {
+        tieScore.innerText++
+    } else if (computerChoice == 2) {
+        computerScore.innerText++
+    } else {
+        playerScore.innerText++
+    }
+})
+var scissorOption = document.querySelector(".scissor")
+scissorOption.addEventListener("click", function () {
+    var computerChoice = Math.floor(Math.random() * 3);
+    var computerPick = document.getElementById('computer-choice')
+    if (computerChoice == 0) {
+        computerPick.setAttribute('src', 'rock.jpg')
+        document.getElementById('computer-message').innerText = "Computer picked ROCK"
+    } else if (computerChoice == 1) {
+        computerPick.setAttribute('src', 'paper.jpg')
+        document.getElementById('computer-message').innerText = "Computer picked PAPER"
+    } else {
+        computerPick.setAttribute('src', 'scissor.jpg')
+        document.getElementById('computer-message').innerText = "Computer picked SCISSOR"
+    }
+    playerChoice = 2;
+    document.getElementById('player-message').innerText = "You picked SCISSOR"
+    if (computerChoice == playerChoice) {
+        tieScore.innerText++
+    } else if (computerChoice == 0) {
+        computerScore.innerText++
+    } else {
+        playerScore.innerText++
+    }
+})
