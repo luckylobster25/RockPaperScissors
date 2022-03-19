df379a147e5f6e25fa3f0b27fac995112238ed73
Chue Chang <chuechang1996@outlook.com>
2022-03-19 15:06:45 -0500

    add readme and comments

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# RockPaperScissors
+# Rock🪨 Paper📃 Scissors✂️
+## Introduction:  
+A very popular game among youngsters. This is a very easy game to play. In this application, scores are being tracked. Player will be able to see how many time they win against computer or tie. Player also have the option to reset score and start over again.  
+*******
+HERE IS THE LINK TO GAME. [ROCK-PAPER-SCISSORS](https://luckylobster25.github.io/RockPaperScissors/)  
+*******
+![](asset/overview.jpg)
+## Tool used: 
+Everything in this project are done using HTML, CSS and Front-end Javascript. No packages are used. 
+## Contribution:  
+This is a individual project created by Chue Chang. You are than welcome to improve this application.  
--- asset/script.js
+++ asset/script.js
@@ -1,9 +1,11 @@
+// To display or to hide rules
 function hideShow() {
     var x = document.querySelector(".rule-message-box")
     if (x.style.display === "none") {
         x.style.display = "block";
     } else x.style.display = "none"
 }
+
 var playerScore = document.getElementById("player-score")
 playerScore.innerHTML = 0
 var computerScore = document.getElementById("computer-score")
@@ -11,12 +13,14 @@ computerScore.innerHTML = 0
 var tieScore = document.getElementById("tie-score")
 tieScore.innerHTML = 0
 var playerChoice = ""
-var computerChoice = Math.floor(Math.random() * 3)
+var computerChoice = Math.floor(Math.random() * 3) //randomize a number from 0 up to 3 but not 3.
 
+// functionality for when player click on rock image
 var rockOption = document.querySelector(".rock")
 rockOption.addEventListener("click", function () {
     var computerChoice = Math.floor(Math.random() * 3);
     var computerPick = document.getElementById('computer-choice')
+    // if statement to display the correct image for computer's choice. 
     if (computerChoice == 0) {
         computerPick.setAttribute('src', 'rock.jpg')
         document.getElementById('computer-message').innerText = "Computer picked ROCK"
@@ -29,6 +33,7 @@ rockOption.addEventListener("click", function () {
     }
     playerChoice = 0;
     document.getElementById('player-message').innerText = "You picked ROCK"
+    // comparing and scoring according
     if (computerChoice == playerChoice) {
         tieScore.innerText++
         document.querySelector(".winner-loser").innerText = "It is a Tie!"
@@ -40,6 +45,7 @@ rockOption.addEventListener("click", function () {
         document.querySelector(".winner-loser").innerText = "You Win!"
     }
 })
+// functionality for when player click on paper image
 var paperOption = document.querySelector(".paper")
 paperOption.addEventListener("click", function () {
     var computerChoice = Math.floor(Math.random() * 3);
@@ -68,6 +74,7 @@ paperOption.addEventListener("click", function () {
         document.querySelector(".winner-loser").innerText = "You Win!"
     }
 })
+// functionality for when player click on scissor image
 var scissorOption = document.querySelector(".scissor")
 scissorOption.addEventListener("click", function () {
     var computerChoice = Math.floor(Math.random() * 3);
@@ -95,6 +102,7 @@ scissorOption.addEventListener("click", function () {
         document.querySelector(".winner-loser").innerText = "You Win!"
     }
 })
+// reset button, which reset all score being tracked. 
 var resetBtn = document.querySelector(".reset").addEventListener("click", function () {
     playerScore.innerText = "0"
     computerScore.innerText = "0"
